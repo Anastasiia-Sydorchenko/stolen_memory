9ecc6cf71ba028216a625664f38ad06349dc14c0
Anastasiia <nsydorchenko@gmail.com>
2021-04-29 14:00:19 +0300

    add slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,37 @@
-// // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
+// import Swiper JS
+import Swiper from 'swiper';
 
-// // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
+// core version + navigation, pagination modules:
+import SwiperCore, { Navigation, Pagination, Mousewheel } from 'swiper/core';
 
-document.addEventListener('DOMContentLoaded', () => {
+// configure Swiper to use modules
+SwiperCore.use([Navigation, Pagination, Mousewheel]);
 
-	// Custom JS
+const swiper = new Swiper('.people_gallery__slider', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+   slidesPerView: 2,
+   spaceBetween: 20,
+   lazyLoading: true,
+   mousewheel: {
+      invert: true,
+   },
+   breakpoints: {   
+      991: {
+         slidesPerView: 3,
+         spaceBetween: 35
+      },
 
-})
+      1599: {
+         slidesPerView: 4,
+         spaceBetween: 35
+      },
+   },
+
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   }
+ });
+ 
