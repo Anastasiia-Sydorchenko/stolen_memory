d4fe87964061d0e291195f0d619a9aa4a75048da
Anastasiia <nsydorchenko@gmail.com>
2021-05-11 15:10:20 +0300

    100vh mobile fix

--- app/js/app.js
+++ app/js/app.js
@@ -49,3 +49,16 @@ var $page = $('html, body');
      }, 800);
      return false;
 });
+
+// 100VH MOBILE FIX
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener, ('resize', () => {
+   // We execute the same script as before
+   let vh = window.innerHeight * 0.01;
+   document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
