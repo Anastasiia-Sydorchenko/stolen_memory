dd69d0a9641fe5b64329a82e15b6ca52d4a52992
Anastasiia <nsydorchenko@gmail.com>
2021-05-13 14:51:26 +0300

    fix slider mousewheel, fix footer form, improve adaptivity

--- app/js/app.js
+++ app/js/app.js
@@ -13,19 +13,19 @@ import SwiperCore, { Navigation, Pagination, Mousewheel } from 'swiper/core';
 SwiperCore.use([Navigation, Pagination, Mousewheel]);
 
 // 100VH MOBILE FIX
-
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// We listen to the resize event
 window.addEventListener('resize', () => {
-   // We execute the same script as before
    let vh = window.innerHeight * 0.01;
    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+   if (window.innerWidth < 1025) {
+      swiperPeople.mousewheel.disable();
+   }
 });
 
+// PEOPLE GALLERY SLIDER
 const swiperPeople = new Swiper('.people_gallery__slider', {
    // Optional parameters
    direction: 'horizontal',
@@ -64,6 +64,7 @@ const swiperPeople = new Swiper('.people_gallery__slider', {
    }
 });
 
+// OBJECTS GALLERY SLIDER
 const swiperItems = new Swiper('.objects-gallery__mobile', {
    // Optional parameters
    direction: 'horizontal',
